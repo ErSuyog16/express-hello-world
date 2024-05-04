cbb11ed049ba8e3cc664ca1635e12d101d79c4b0
Suyog <89405908+ErSuyog16@users.noreply.github.com>
2024-05-04 21:25:23 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,1 +1,34 @@
 
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let newsAdmin = new Schema(
+  {
+    Title: {
+      type: String,
+    },
+    ImageUrl: {
+      type: String,
+    },
+    Description: {
+      type: String,
+    },
+    ReadMoreUrl: {
+      type: String,
+    },
+    ReadMoreText: {
+      type: String,
+    },
+    formDate:{
+      type:Date
+    },
+    formTime:{
+      type:Date
+    }
+  },
+  {
+    collection: "News",
+  }
+);
+
+module.exports = mongoose.model("News", newsAdmin);
